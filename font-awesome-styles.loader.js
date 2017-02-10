60e6edcb1db9a044353bb7abd74622e53354dd13
David Furlong <dvfurlong@googlemail.com>
2017-02-10 17:21:03 +0000

    Update font-awesome-styles.loader.js

--- font-awesome-styles.loader.js
+++ font-awesome-styles.loader.js
@@ -17,8 +17,8 @@ module.exports = function(content) {
     this.cacheable(true);
     var config = this.exec(content, this.resourcePath);
     var start =
-            "@import          \"~font-awesome/less/variables.less\";\n"
-            + "@fa-font-path: \"~font-awesome/fonts/\";\n"
+            "@import          \"~/node_modules/font-awesome/less/variables.less\";\n"
+            + "@fa-font-path: \"~/node_modules/font-awesome/fonts/\";\n"
             + "@import          \"./font-awesome.config.less\";\n";
     source = start + styles.filter(function(style) {
         return config.styles[style];
